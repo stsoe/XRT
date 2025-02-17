6cd0def75efaa6728291a0027871ff496a120b81
Soren Soe <2106410+stsoe@users.noreply.github.com>
2025-02-17 09:19:08 -0800

    Leverage that Visual Studio supports multi-configuration builds
    Rather than building separate packages for Release and Debug, support
    combining both configurations into one build.
    
    This is particularly useful when creating packages with CPack.  The
    XRT package can combine both Release and Debug binaries provided the
    binaries are named differently.  One installer supports both
    configurations.
    
    In order for this to work the changes use CMAKE_DEBUG_POSTFIX so that
    all libraries are appended with 'd' in debug.  Similarly all
    executables use a DEBUG_POSTFIX in target properties to append "_d" to
    the executable name when build in Debug.
    
    The Windows installer created from an XRT build (for npu or alveo) now
    contains both release and debug binaries but only one copy of shared
    content.
    
    To this extend, the build script (currently only build-win22.sh) was
    modified to combine the Debug and Release builds under the same CMake
    configuration.  The build options are:
    
    ```
    % build22.bat [-dbg|-opt|-nobuild] [-sdk]
       [-dbg]      Build debug library (default)
       [-opt]      Build release library (default)
       [-nobuild]  Disables build step
       [-sdk]      Create NSIS XRT SDK NPU Installer (requires NSIS installed).
    ```
    
    By default the build script builds both Release and Debug profiles, if
    only one profile is desired, then specify either `-opt` or
    `-dbg`. Since these two switches disable the other profile, they
    cannot both be given at the same time.  The `-nobuild` switch turns of
    all building and cannot be used with `-dbg` or `-opt`.
    
    The packaging step is controlled by the `-sdk` switch.  It can be used
    without without any of `-opt`, `-dbg`, or `-nobuild`.  It will package
    the content currently built.  In order for the package to be useful,
    the `-sdk` witch should probably only be used after both Release and
    Debug profile have been built.
    
    CMake's find_package works correctly for both Release and Debug
    applications, for example:
    
    ```
    % cat CMakeLists.txt
    ...
    find_package(XRT REQUIRED HINTS ${XILINX_XRT}/share/cmake/XRT)
    add_executable(verify verify.cpp)
    target_link_libraries(verify XRT::xrt_coreutil)
    
    build% cmake -DXILINX_XRT=C:\Program Files\XRT\xrt ..
    build% cmake --build . --config Release
    build% cmake --build . --config Debug
    ```
    
    The risk with this PR is that the debugging experience for native
    commandline Windows users has changed.  Manual linking of debug
    must use `<library>d.lib` during linking.  But CMake is seamless.
    
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- build/build22.bat
+++ build/build22.bat
@@ -1,22 +1,26 @@
 @ECHO OFF
 
 REM Copyright (C) 2022 Xilinx, Inc
+REM Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
 REM SPDX-License-Identifier: Apache-2.0
+setlocal enabledelayedexpansion
 set SCRIPTDIR=%~dp0
+set SCRIPTDIR=%SCRIPTDIR:~0,-1%
 set BUILDDIR=%SCRIPTDIR%
 
 set DEBUG=1
 set RELEASE=1
 set EXT_DIR=C:/Xilinx/XRT/ext.new
-set CREATE_PACKAGE=0
-set CREATE_SDK=0
+set SDK=0
 set CMAKEFLAGS=
+set CLEAN=0
+set NOINIT=0
 set NOCMAKE=0
+set NOBUILD=0
+set BASE=0
+set NPU=0
+set ALVEO=0
 set NOCTEST=0
-set XCLMGMT_DRIVER=
-set XCLMGMT2_DRIVER=
-set XOCLUSER_DRIVER=
-set XOCLUSER2_DRIVER=
 
 IF DEFINED MSVC_PARALLEL_JOBS ( SET LOCAL_MSVC_PARALLEL_JOBS=%MSVC_PARALLEL_JOBS%) ELSE ( SET LOCAL_MSVC_PARALLEL_JOBS=3 )
 
@@ -25,78 +29,134 @@ IF DEFINED MSVC_PARALLEL_JOBS ( SET LOCAL_MSVC_PARALLEL_JOBS=%MSVC_PARALLEL_JOBS
     goto argsParsed
   ) else (
   if [%1] == [-clean] (
-    goto Clean
+    SET CLEAN=1
   ) else (
   if [%1] == [-help] (
     goto Help
   ) else (
   if [%1] == [-dbg] (
+    if [%DEBUG%] == [0] (
+      echo build22.bat: -dbg and -opt are mutually exclusive, specify one or none
+      exit /B 1
+    )
+    set DEBUG=1
+    set RELEASE=0
+  ) else (
+  if [%1] == [-opt] (
+    if [%RELEASE%] == [0] (
+      echo build22.bat: -opt and -dbg are mutually exclusive, specify one or none
+      exit /B 1
+    )
+    set RELEASE=1
+    set DEBUG=0
+  ) else (
+  if [%1] == [-nobuild] (
+    if [%DEBUG%] == [0] (
+      echo build22.bat: -nobuild cannot be used -dbg or -opt
+      exit /B 1
+    )
+    if [%RELEASE%] == [0] (
+      echo build22.bat: -nobuild cannot be used -opt or -dbg
+      exit /B 1
+    )
+    set DEBUG=0
     set RELEASE=0
   ) else (
   if [%1] == [-ext] (
     shift
-    set EXT_DIR=%2
+    set EXT_DIR=%1
   ) else (
-  if [%1] == [-opt] (
-    set DEBUG=0
+  if [%1] == [-base] (
+    set BASE=1
+    set CMAKEFLAGS=%CMAKEFLAGS% -DXRT_BASE=1
   ) else (
   if [%1] == [-npu] (
+    set NPU=1
     set CMAKEFLAGS=%CMAKEFLAGS% -DXRT_NPU=1
+  ) else (
+  if [%1] == [-alveo] (
+    set ALVEO=1
+    set CMAKEFLAGS=%CMAKEFLAGS% -DXRT_ALVEO=1
+  ) else (
+  if [%1] == [-noinit] (
+    set NOINIT=1
+  ) else (
+  if [%1] == [-nocmake] (
+    set NOCMAKE=1
+    set NOINIT=1
   ) else (
   if [%1] == [-noabi] (
     set CMAKEFLAGS=%CMAKEFLAGS% -DDISABLE_ABI_CHECK=1
   ) else (
   if [%1] == [-sdk] (
-    set CREATE_SDK=1
-    set CMAKEFLAGS=%CMAKEFLAGS% -DXRT_NPU=1
-  ) else (
-  if [%1] == [-pkg] (
-    set CREATE_PACKAGE=1
-  ) else (
-  if [%1] == [-xclmgmt] (
-    set XCLMGMT_DRIVER=%2
-    shift
-  ) else (
-  if [%1] == [-xocluser] (
-    set XOCLUSER_DRIVER=%2
-    shift
-  ) else (
-  if [%1] == [-xclmgmt2] (
-    set XOCLMGMT2_DRIVER=%2
-    shift
-  ) else (
-  if [%1] == [-xocluser2] (
-    set XOCLUSER2_DRIVER=%2
-    shift
-  ) else (
-  if [%1] == [-nocmake] (
-    set NOCMAKE=1
+    set SDK=1
   ) else (
   if [%1] == [-hip] (
     set CMAKEFLAGS=%CMAKEFLAGS% -DXRT_ENABLE_HIP=ON
   ) else (
     echo Unknown option: %1
     goto Help
-  ))))))))))))))))
+  )))))))))))))))
   shift
   goto parseArgs
 
 :argsParsed
 
-set CMAKEFLAGS=%CMAKEFLAGS% -DMSVC_PARALLEL_JOBS=%LOCAL_MSVC_PARALLEL_JOBS% -DKHRONOS=%EXT_DIR% -DBOOST_ROOT=%EXT_DIR% -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-ECHO CMAKEFLAGS=%CMAKEFLAGS%
+if [%CLEAN%] == [1] (
+  if EXIST %BUILDDIR%\WBuild (
+    echo Removing 'WBuild' directory...
+    rmdir /S /Q %BUILDDIR%\WBuild
+  )
+  exit /B 0
+)
+
+if [%NPU%+%ALVEO%+%BASE%] gtr 1 (
+  echo build22.bat: -npu, -alveo, -base are mutually exclusive
+  exit /B 1
+)
+
+if [%NOCMAKE%] == [0] if EXIST %BUILDDIR%\WBuild\CMakeCache.txt (
+    echo Disabling cmake configuration; using existing build configuration
+    echo Use -clean to remove existing build configuration
+    set NOCMAKE=1
+    set NOINIT=1
+)
+
+if [%NOINIT%] == [0] (
+   echo Updating Git submodules, use -noinit option to avoid updating
+   git submodule update --init --progress --recursive
+)
+
+if [%NOCMAKE%] == [0] (
+   echo Configuring CMake project
+   set CMAKEFLAGS=%CMAKEFLAGS%^
+   -DMSVC_PARALLEL_JOBS=%LOCAL_MSVC_PARALLEL_JOBS%^
+   -DKHRONOS=%EXT_DIR%^
+   -DBOOST_ROOT=%EXT_DIR%^
+   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON^
+   -DCMAKE_INSTALL_PREFIX=%BUILDDIR%\WBuild\xilinx
+
+   echo cmake -B %BUILDDIR%\WBuild -G "Visual Studio 17 2022" !CMAKEFLAGS! %BUILDDIR%\..\src
+   cmake -B %BUILDDIR%\WBuild -G "Visual Studio 17 2022" !CMAKEFLAGS! %BUILDDIR%\..\src
+)
 
 if [%DEBUG%] == [1] (
-   call :DebugBuild
-   if errorlevel 1 (exit /B %errorlevel%)
+   echo cmake --build %BUILDDIR%\WBuild --config Debug --target install --verbose
+   cmake --build %BUILDDIR%\WBuild --config Debug --target install --verbose
 )
 
 if [%RELEASE%] == [1] (
-   call :ReleaseBuild
-   if errorlevel 1 (exit /B %errorlevel%)
+   echo cmake --build %BUILDDIR%\WBuild --config Release --target install --verbose
+   cmake --build %BUILDDIR%\WBuild --config Release --target install --verbose
 )
 
-goto :EOF
+if [%SDK%] == [1] (
+   echo Create SDK NSIS Installer ...
+   PUSHD %BUILDDIR%\WBuild
+   cpack -G NSIS -C Release;Debug
+)
+
+exit /b 0
 
 REM --------------------------------------------------------------------------
 :Help
@@ -105,99 +165,14 @@ ECHO Usage: build22.bat [options]
 ECHO.
 ECHO [-help]                    - List this help
 ECHO [-clean]                   - Remove build directories
-ECHO [-dbg]                     - Creates a debug build
+ECHO [-dbg]                     - Creates a debug build (default)
+ECHO [-opt]                     - Creates a release build (default)
+ECHO [-nocmake]                 - Do not reconfigure the project (implies -noinit)
+ECHO [-nobuild]                 - Do not build the project
+ECHO [-noinit]                  - Do not initialize submodules
 ECHO [-noabi]                   - Do not compile with ABI version check (make incremental builds faster)
-ECHO [-opt]                     - Creates a release build
 ECHO [-sdk]                     - Create NSIS XRT SDK Installer for NPU (requires NSIS installed).
-echo [-package]                 - Packages the release build to a MSI archive.
-ECHO                              Note: Depends on the WIX application. 
-ECHO [-xclmgmt arg]             - The directory to the xclmgmt drivers (used with [-package])
-ECHO [-xocluser arg]            - The directory to the xocluser drivers (used with [-package])
-ECHO [-xclmgmt2 arg]            - The directory to the xclmgmt2 drivers (used with [-package])
-ECHO [-xocluser2 arg]           - The directory to the xocluser2 drivers (used with [-package])
+ECHO [-alveo]                   - Build Alveo component of XRT (deployment and development)
 ECHO [-npu]                     - Build NPU component of XRT (deployment and development)
 ECHO [-hip]                     - Enable hip library build
-GOTO:EOF
 
-REM --------------------------------------------------------------------------
-:Clean
-PUSHD %BUILDDIR%
-IF EXIST WDebug (
-  ECHO Removing 'WDebug' directory...
-  rmdir /S /Q WDebug
-)
-IF EXIST WRelease (
-  ECHO Removing 'WRelease' directory...
-  rmdir /S /Q WRelease
-)
-POPD
-GOTO:EOF
-
-REM --------------------------------------------------------------------------
-:DebugBuild
-echo ====================== Windows Debug Build ============================
-set CMAKEFLAGS=%CMAKEFLAGS% -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%BUILDDIR%/WDebug/xilinx
-ECHO CMAKEFLAGS=%CMAKEFLAGS%
-
-MKDIR %BUILDDIR%\WDebug
-PUSHD %BUILDDIR%\WDebug
-
-if [%NOCMAKE%] == [0] (
-   cmake -G "Visual Studio 17 2022" %CMAKEFLAGS% ../../src
-   IF errorlevel 1 (POPD & exit /B %errorlevel%)
-)
-
-cmake --build . --verbose --config Debug --target install
-IF errorlevel 1 (POPD & exit /B %errorlevel%)
-
-POPD
-GOTO:EOF
-
-REM --------------------------------------------------------------------------
-:ReleaseBuild
-ECHO ====================== Windows Release Build ============================
-set CMAKEFLAGS=%CMAKEFLAGS% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%BUILDDIR%/WRelease/xilinx
-
-MKDIR %BUILDDIR%\WRelease
-PUSHD %BUILDDIR%\WRelease
-
-IF NOT [%XCLMGMT_DRIVER%] == [] (
-  ECHO Packaging xclbmgmt driver directory: %XCLMGMT_DRIVER%
-  set CMAKEFLAGS=%CMAKEFLAGS% -DXCL_MGMT=%XCLMGMT_DRIVER%
-)
-
-IF NOT [%XCLMGMT2_DRIVER%] == [] (
-  ECHO Packaging xclbmgmt2 driver directory: %XCLMGMT2_DRIVER%
-  set CMAKEFLAGS=%CMAKEFLAGS% -DXCL_MGMT2=%XCLMGMT2_DRIVER%
-)
-
-IF NOT [%XOCLUSER_DRIVER%] == [] (
-  ECHO Packaging xocluser directory: %XOCLUSER_DRIVER%
-  set CMAKEFLAGS=%CMAKEFLAGS% -DXOCL_USER=%XOCLUSER_DRIVER%
-)
-
-IF NOT [%XOCLUSER2_DRIVER%] == [] (
-  ECHO Packaging xocluser2 directory: %XOCLUSER2_DRIVER%
-  set CMAKEFLAGS=%CMAKEFLAGS% -DXOCL_USER2=%XOCLUSER2_DRIVER%
-)
-
-ECHO CMAKEFLAGS=%CMAKEFLAGS%
-
-if [%NOCMAKE%] == [0] (
-   cmake -G "Visual Studio 17 2022" %CMAKEFLAGS% ../../src
-   IF errorlevel 1 (POPD & exit /B %errorlevel%)
-)
-
-cmake --build . --verbose --config Release --target install
-IF errorlevel 1 (POPD & exit /B %errorlevel%)
-
-ECHO ====================== Zipping up Installation Build ============================
-cpack -G ZIP -C Release
-
-if [%CREATE_SDK%] == [1] (
-  ECHO ====================== Creating SDK Installer ============================
-  cpack -G NSIS -C Release
-)
-
-popd
-GOTO:EOF
